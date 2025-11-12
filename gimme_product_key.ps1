7dae48ad4685150f0684806deb514a176ce8d282
Eitan Biletski <87935299+shaijyy@users.noreply.github.com>
2025-11-12 20:45:34 +0200

    Update Script No. 1 To Include Title :)

--- gimme_product_key.ps1
+++ gimme_product_key.ps1
@@ -1,1 +1,2 @@
+Write-Host "=== Gimme Product Key ===" -ForegroundColor Cyan
 echo "Sure bro! here's ur Product Key:" (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
